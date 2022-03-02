ca08029687d5be6964e70d185cf27d0139b65632
abhinav3254 <abhinav3254@gmail.com>
2022-03-02 21:56:36 +0530

    making own api

--- complicated_api.py
+++ complicated_api.py
@@ -8,6 +8,11 @@ app.config["Debug"] = True
 # create some data for our catalog in the form of a list of dictinories
 
 books = [
+    {'id': 'admin',
+     'title': 'Hey This is Abhinav',
+     'author': 'Abhinav Kumar',
+     'first_sentence': 'Welcome to My API',
+     'year_published': '02/03/2022'},
     {'id': 0,
      'title': 'A Fire Upon the Deep',
      'author': 'Vernor Vinge',
@@ -25,14 +30,38 @@ books = [
      'published': '1975'}
 ]
 
+numbers = [
+    {
+        'id' : '1',
+        'name':'abhinav'
+    },
+    {
+        'id' : '2',
+        'name':'ajay'
+    },
+    {
+        'id' : '3',
+        'name':'aakash'
+    },
+    {
+        'id' : '4',
+        'name':'aman'
+    },
+    
+]
+
 @app.route('/',methods=['GET'])
 def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-@app.route('/api/v1/resources/books/all',methods=['GET'])
-def api_all():
-    return jsonify(books)
+# @app.route('/api/v1/resources/books/all',methods=['GET'])
+# def api_all():
+#     return jsonify(books)
+
+@app.route('/num',methods=['GET'])
+def api_one():
+    return jsonify(numbers)
 
 app.run()
